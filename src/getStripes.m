90ca42867e8a9886b5b75ed787ad16e216d2bbdb
Henrique Weber <iqueweber@gmail.com>
2016-10-07 13:54:36 -0400

    getStripes.m updated

--- src/getStripes.m
+++ src/getStripes.m
@@ -1,5 +1,22 @@
 function [stripes_img] = getStripes(input_img_name)
+ 
+ %load input_image
  input_img = imread(input_img_name);
+ 
+ %resize input_imgimage
+ input_img = imresize(input_img,.4);
+ 
+ %create stripes_img with the same size as the input
  stripes_img = zeros(size(input_img));
+ 
+ %convert to grayscale
+ input_img = rgb2gray(input_img);
+ 
+ f = @(x) (x(2,2) > x(1,2) && x(2,2) > x(3,2))*255;
+ 
+ stripes_img  = nlfilter(input_img,[3 3],f);
+   
  imshow(stripes_img);
+ 
+ 
 end
